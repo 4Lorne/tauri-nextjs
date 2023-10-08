a54a8771da9611d8b41b974e195fef925b7bc802
Lorne Day <77252924+4Lorne@users.noreply.github.com>
2023-10-08 19:10:58 -0300

    Initial commit

---
+++ src/app/(pages)/About/page.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <header className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hello World!
+    </header>
+  );
+};
+
+export default About;
