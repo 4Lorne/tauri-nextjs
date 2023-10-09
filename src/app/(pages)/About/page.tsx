46f4832c57504854837c22a3c45d2f0ac0496c4d
Lorne Day <77252924+4Lorne@users.noreply.github.com>
2023-10-09 01:09:25 -0300

    Markdown working

--- src/app/(pages)/About/page.tsx
+++ src/app/(pages)/About/page.tsx
@@ -1,8 +1,34 @@
-const About = () => {
+"use client";
+import { Dispatch, SetStateAction, useState } from "react";
+import ReactMarkdown from "react-markdown";
+
+const TextArea = ({
+  setText,
+}: {
+  setText: Dispatch<SetStateAction<string>>;
+}) => {
   return (
-    <header className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World!
-    </header>
+    <>
+      <textarea
+        id="editor"
+        className="text-black"
+        onChange={(e) => setText(e.target.value)}
+      />
+    </>
+  );
+};
+
+const About = () => {
+  const [text, setText] = useState("");
+  console.log(text);
+
+  return (
+    <div className="flex-end flex">
+      <TextArea setText={setText} />
+      <ReactMarkdown className={"prose bg-red-500 text-red-200"}>
+        {text}
+      </ReactMarkdown>
+    </div>
   );
 };
 
