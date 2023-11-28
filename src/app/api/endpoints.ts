16adcd9899ed53696f246c3b340b7c79ba0f716e
Lorne Day <77252924+4Lorne@users.noreply.github.com>
2023-11-28 16:05:20 -0400

    endpoints enum

---
+++ src/app/api/endpoints.ts
@@ -0,0 +1,11 @@
+export enum ENDPOINTS {
+  // GET
+  GET_DATA = "/api/hooks/useGetData",
+  GET_LIST = "/api/hooks/useGetList",
+
+  //POST
+  POST_DATA = "/api/hooks/useUploadData",
+
+  //PUT
+  PUT_DATA = "/api/hooks/useUpdateData",
+}
