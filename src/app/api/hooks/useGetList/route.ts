3f81d5782a625c9e09286f865fc6c6fe6fe95af6
Lorne Day <77252924+4Lorne@users.noreply.github.com>
2023-12-01 15:40:32 -0400

    Testing fetching

--- src/app/api/hooks/useGetList/route.ts
+++ src/app/api/hooks/useGetList/route.ts
@@ -3,14 +3,14 @@ import { NextResponse } from "next/server";
 
 const getList = async () => {
   try {
-    // const result = await sql`SELECT id, filename, file_data from files`;
-    //
-    // return new NextResponse(JSON.stringify(result), {
-    //   status: 200,
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // });
+    const result = await sql`SELECT id, filename, file_data from files`;
+    console.log(result);
+    return new NextResponse(JSON.stringify(result), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   } catch (err) {
     console.error("Error retrieving data:", err);
     throw err;
